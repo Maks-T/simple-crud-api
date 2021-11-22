e9e11f006944b8b66dd08668fc3d364e490de7f4
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2021-11-22 22:39:56 +0300

    feat: add get handler

--- src/database/db.js
+++ src/database/db.js
@@ -5,6 +5,12 @@ const db = [
     age: 32,
     hobbies: ["programming", "reading", "science"],
   },
+  {
+    id: "dc375068-4ae6-11ec-81d3-0242ac130001",
+    name: "Karina",
+    age: 32,
+    hobbies: ["programming", "reading", "science"],
+  },
 ];
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
+const { db } = require("./database/db");
 
 const hostname = "127.0.0.1";
 
@@ -10,7 +11,11 @@ process.env.PORT = configApp.PORT;
 
 const port = process.env.PORT || 3001;
 
-const server = http.createServer((request, response) => {
+const r = "\x1b[31m";
+const w = "\x1b[37m";
+const y = "\x1b[33m";
+
+const server = http.createServer((req, res) => {
   /*
   
     response.setHeader("UserId", 12);
@@ -18,21 +23,25 @@ const server = http.createServer((request, response) => {
   response.write("<h2>hello world</h2>");
   response.end();
 */
-  switch (request.method) {
-    case "GET": {
-      console.log(request.url);
+  console.log(`${y}Method ${req.method}, url = '${req.url}'${w}`);
 
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(
-        JSON.stringify({
-          data: "Hello World!",
-        })
-      );
-    }
+  //const url = new URL(path.join(req.host, req.url).toString());
+
+  console.log("req.host", req.host);
+
+  if (req.url === "/person" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(db));
+  } else if (req.url.match(/\/person\/\w+/) && req.method === "GET") {
+    const id = req.url.split("/")[2];
+    findPerson = db.find((person) => person.id == id);
+    res.end(JSON.stringify(findPerson));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Error: Invalid Request" }));
   }
 });
 
 server.listen(port, hostname, () => {
-  console.log("process.env.PORT", process.env.PORT);
   console.log(`Server running at http://${hostname}:${port}/`);
 });
